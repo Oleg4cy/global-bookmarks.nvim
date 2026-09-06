64891dce1596fda1dcd69f515a7a964434df59b3
techoleg <semenikhinoleg777@yandex.ru>
2026-09-06 19:01:40 +0300

    feat: add built-in nvim-tree bookmark integration

--- lua/global-bookmarks/integrations/nvim-tree.lua
+++ lua/global-bookmarks/integrations/nvim-tree.lua
@@ -1,8 +1,50 @@
 local M = {}
 
+local bookmark_cache_generation = 0
+
+local function invalidate_bookmark_cache()
+  bookmark_cache_generation = bookmark_cache_generation + 1
+end
+
+local function setup_highlights()
+  vim.api.nvim_set_hl(0, "GlobalBookmarksNvimTreeHL", { fg = "#fb4934", bold = true, default = true })
+  vim.api.nvim_set_hl(0, "GlobalBookmarksNvimTreeIcon", { fg = "#fb4934", bold = true, default = true })
+end
+
+function M.attach(bufnr)
+  if type(bufnr) ~= "number" or not vim.api.nvim_buf_is_valid(bufnr) then
+    return false
+  end
+
+  local function opts(desc)
+    return { buffer = bufnr, silent = true, nowait = true, desc = "nvim-tree: " .. desc }
+  end
+
+  vim.keymap.set("n", "<Plug>(GlobalBookmarksNvimTreeToggle)", M.toggle_node, opts("Toggle Global Bookmark"))
+  vim.keymap.set("n", "<Plug>(GlobalBookmarksNvimTreeOpen)", "<Cmd>GlobalBookmarks<CR>", opts("Open Global Bookmarks"))
+
+  vim.api.nvim_buf_call(bufnr, function()
+    if vim.fn.hasmapto("<Plug>(GlobalBookmarksNvimTreeToggle)", "n") == 0
+      and vim.fn.maparg("gm", "n") == ""
+    then
+      vim.keymap.set("n", "gm", "<Plug>(GlobalBookmarksNvimTreeToggle)", opts("Toggle Global Bookmark"))
+    end
+
+    if vim.fn.hasmapto("<Plug>(GlobalBookmarksNvimTreeOpen)", "n") == 0
+      and vim.fn.maparg("gb", "n") == ""
+    then
+      vim.keymap.set("n", "gb", "<Plug>(GlobalBookmarksNvimTreeOpen)", opts("Open Global Bookmarks"))
+    end
+  end)
+
+  return true
+end
+
 function M.decorator()
   local api = require("nvim-tree.api")
   local bookmarks = require("global-bookmarks")
+
+  setup_highlights()
 
   local GlobalBookmarkDecorator = api.Decorator:extend()
 
@@ -15,11 +57,17 @@ function M.decorator()
       hl = { "GlobalBookmarksNvimTreeIcon" },
     }
     self.bookmark_cache = {}
+    self.bookmark_cache_generation = bookmark_cache_generation
   end
 
   function GlobalBookmarkDecorator:is_bookmarked(node)
     if node == nil or node.absolute_path == nil or node.absolute_path == "" then
       return false
+    end
+
+    if self.bookmark_cache_generation ~= bookmark_cache_generation then
+      self.bookmark_cache = {}
+      self.bookmark_cache_generation = bookmark_cache_generation
     end
 
     local path = node.absolute_path
@@ -52,6 +100,8 @@ function M.decorator()
 end
 
 function M.refresh()
+  invalidate_bookmark_cache()
+
   local api = package.loaded["nvim-tree.api"]
   if type(api) ~= "table" or not api.tree.is_visible() then
     return false
